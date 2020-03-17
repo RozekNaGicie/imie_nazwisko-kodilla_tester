57cdf511843299aae66f7ecd0bee15df7210a941
Mateusz Narozny <mat.narozny95@gmail.com>
2020-03-18 00:18:31 +0200

    cwiczenia

---
+++ Podstawy-testow-java/src/main/java/Loops.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+
+public class Loops {
+
+    public int main(int[] array) {
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+
+            sum = sum + array[i];
+
+        }
+
+        return sum;
+
+    }
+
+}
--- Podstawy-testow-java/src/main/java/SimpleArray.java
+++ Podstawy-testow-java/src/main/java/SimpleArray.java
@@ -2,17 +2,33 @@ import java.lang.reflect.Array;
 
 public class SimpleArray {
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
-        String newArray[] = new String[] {"Pies","Kot", "Slon","Jaszczomp", "Myszojelen","Bober"};
-
+        String newArray[] = new String[]{"Pies", "Kot", "Slon", "Jaszczomp", "Myszojelen", "Bober"};
         String newAnimal = newArray[3];
+
         System.out.println(newAnimal);
 
         //pobieranie dlugosci tablicy 2 sposoby
         int someInt = Array.getLength(newArray);
         int someInt2 = newArray.length;
+
         System.out.println("Moja tablica zawiera " + someInt + " elementow");
+
+        // petle
+        String names[] = new String[]{"Zygfryd", "Gwidon", "Florentyna"};
+
+        for (int i = names.length - 1; i >= 0; i--) {
+            System.out.println(names[i]);
+        }
+
+        // wywolanie klasy Loops;
+        Loops arr = new Loops();
+
+        int newTab[] = new int[]{1, 2, 3, 4, 5, 6};
+        int sum = arr.main(newTab);
+
+        System.out.println(sum);
 
     }
 
