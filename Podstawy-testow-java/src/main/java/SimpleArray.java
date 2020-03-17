eba17233d61bd28d06eb16f1f1b7d1fdea5a28b6
Mateusz Narozny <mat.narozny95@gmail.com>
2020-03-17 23:44:01 +0200

    orig

--- Podstawy-testow-java/src/main/java/SimpleArray.java
+++ Podstawy-testow-java/src/main/java/SimpleArray.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.lang.reflect.Array;
 
 public class SimpleArray {
+
+    public static void main (String[] args){
+
+        String newArray[] = new String[] {"Pies","Kot", "Slon","Jaszczomp", "Myszojelen","Bober"};
+
+        String newAnimal = newArray[3];
+        System.out.println(newAnimal);
+
+        //pobieranie dlugosci tablicy 2 sposoby
+        int someInt = Array.getLength(newArray);
+        int someInt2 = newArray.length;
+        System.out.println("Moja tablica zawiera " + someInt + " elementow");
+
+    }
+
 }
