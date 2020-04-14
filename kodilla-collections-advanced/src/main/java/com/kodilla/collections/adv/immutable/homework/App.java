bbb66d778ace59080e4efd25dc6aef9dfcc0f91e
Mateusz Narozny <mat.narozny95@gmail.com>
2020-04-14 23:11:05 +0300

    immutable

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/App.java
@@ -0,0 +1,10 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class App {
+
+    public static void main(String[] args) {
+        Task task = new Task("some task", 2);
+//        task.duration = 2; // edycja pola zablokowana;
+//        task.title = "212";
+    }
+}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskInheritance.java
@@ -0,0 +1,16 @@
+//package com.kodilla.collections.adv.immutable.homework;
+
+//public class TaskInheritance extends Task {
+
+//    public String realTitle;
+//
+//    public TaskInheritance(String title, int duration) {
+//        super(title, duration);
+//        realTitle = title;
+//    }
+//
+//    public void modifyTitle(String newTitle) {
+//        realTitle = newTitle;
+//    }
+
+//}
