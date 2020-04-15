98bbecc2a75a241712419164e14fdb6ddfa64da0
Mateusz Narozny <mat.narozny95@gmail.com>
2020-04-16 00:31:09 +0300

    added Set collection instead of List

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -1,24 +1,21 @@
 package com.kodilla.collections.adv.immutable.special.homework;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class BookManager {
 
-    private ArrayList<Book> listOfBooks = new ArrayList<>();
+    private Set<Book> listOfBooks = new HashSet<>();
 
     public Book createBook(String title, String author) {
         Book book = new Book();
         book.setTitle(title);
         book.setAuthor(author);
-        if (listOfBooks.contains(book)) {
-            System.out.println("Ksiazka juz jest na liscie");
-        } else {
-            listOfBooks.add(book);
-        }
+        listOfBooks.add(book);
         return book;
     }
 
-    public ArrayList<Book> getListOfBooks() {
+    public Set<Book> getListOfBooks() {
         return listOfBooks;
     }
 }
