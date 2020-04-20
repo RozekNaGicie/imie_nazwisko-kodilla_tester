757dc86af5ab68309884231eed1556c58ea45f68
Mateusz Narozny <mat.narozny95@gmail.com>
2020-04-21 00:33:12 +0300

    Students and teachers

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/optional/homework/Application.java
@@ -0,0 +1,20 @@
+package com.kodilla.stream.optional.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Application {
+
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Maciej", new Teacher("Adam Don")));
+        students.add(new Student("Wojetek", null));
+        students.add(new Student("Karol", new Teacher("Maciej Bolek")));
+
+        for (Student student : students) {
+            Optional<Teacher> teacher = Optional.ofNullable(student.getTeacher());
+            System.out.println("Imie studenta " + student.getName() + " Imie nauczyciela " + teacher.orElse(new Teacher("<undefined>")).getName());
+        }
+    }
+}
