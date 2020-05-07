bce5dcb4e2d92f7560af16b3f9f9c3bd96dc2880
Mateusz Narozny <mat.narozny95@gmail.com>
2020-05-07 14:56:39 +0300

    @Bean

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/spring_configuration/Animal.java
@@ -0,0 +1,5 @@
+package com.kodilla.spring.spring_configuration;
+
+public interface Animal {
+    String getVoice();
+}
---
+++ kodilla-spring/src/main/java/com/kodilla/spring/spring_configuration/Duck.java
@@ -0,0 +1,9 @@
+package com.kodilla.spring.spring_configuration;
+
+public class Duck implements Animal {
+
+    @Override
+    public String getVoice() {
+        return "Kwa kwa";
+    }
+}
