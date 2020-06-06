6f3906f9b7c40878e7c3bdbba25190c904c6a930
Mateusz Narozny <mat.narozny95@gmail.com>
2020-06-06 09:43:14 +0300

    allegro test suite

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestApp.java
@@ -15,14 +15,20 @@ public class AllegroTestApp {
         WebDriver driver = new ChromeDriver();
         driver.get(url);
 
+        // cookies alert cant be close by alert.accept();
         WebElement fillSearcher = driver.findElement(By.xpath("//*[@slot=\"button\"]/button[2]"));
         fillSearcher.click();
+        fillSearcher = driver.findElement(By.xpath("//*[@class=\"_1h7wt _d25db_UtYE1\"]/div[2]/div[1]/select"));
+        Select categorySelect = new Select(fillSearcher);
+        categorySelect.selectByValue("/kategoria/elektronika");
         fillSearcher = driver.findElement(By.xpath("//*[@class=\"_1h7wt _d25db_UtYE1\"]/input[1]"));
         fillSearcher.sendKeys("mavic mini");
-        fillSearcher = driver.findElement(By.xpath("//*[@class=\"_1h7wt _d25db_UtYE1\"]/div[2]/div[1]/select/optgroup[1]"));
-        Select categorySelect = new Select(fillSearcher);
-        categorySelect.selectByIndex(3);
-        fillSearcher.submit();
+
+        //needs to be click() by selenium
+        WebElement btn = driver.findElement(By.xpath("//*[@class=\"_1h7wt _d25db_UtYE1\"]/button"));
+        btn.click();
+
+        
     }
 
 }
