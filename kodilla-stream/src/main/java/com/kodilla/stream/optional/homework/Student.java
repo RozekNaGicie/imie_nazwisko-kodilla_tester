3eeb28fa3e3e0d94713562da46518771aa7e7662
Mateusz Narozny <mat.narozny95@gmail.com>
2020-06-21 10:24:38 +0300

    fixes

--- kodilla-stream/src/main/java/com/kodilla/stream/optional/homework/Student.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/optional/homework/Student.java
@@ -4,20 +4,24 @@ package com.kodilla.stream.optional.homework;
 import java.util.Optional;
 
 public class Student {
+    private String studentName;
+    private Optional<String> teacher;
 
-    private String name;
-    private Teacher teacher;
-
-    public Student(String name, Teacher teacher) {
-        this.name = name;
-        this.teacher = teacher;
+    public Student(String studentName, Teacher teacher) {
+        this.studentName = studentName;
+        this.teacher = Optional.ofNullable(teacher.getName());
     }
 
-    public String getName() {
-        return name;
+    public Student(String studentName) {
+        this.studentName = studentName;
+        this.teacher = Optional.of("undefined");
     }
 
-    public Teacher getTeacher() {
-        return teacher;
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public String getTeacher() {
+        return String.valueOf(teacher);
     }
 }
